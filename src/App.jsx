cd2fe82a6c90b4d4affe69dee488dbad472b9a03
inamulmd <mdinamulh04@gmail.com>
2025-02-23 21:30:42 +0530

    Initial commit - Added tourism-plan1.0 project

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import React,{useState} from "react";
+import data from './data';
+import Tours from "./components/Tours"
+import './App.css'
+
+
+const App =()=> {
+  
+  const [tours ,setTours] = useState(data)
+
+  function removeTour(id) {
+    const newTours = tours.filter(tour=> tour.id !==id);
+    setTours(newTours);
+  }
+
+  if(tours.length ==0) {
+    return (
+       <div className ="refresh">
+        <h2>No Tours Left</h2>
+        <buttton className="btn-white" onClick={()=> setTours(data)}>
+         Refresh</buttton>
+        </div>
+    )
+  }
+
+  return (
+    <div className="App">
+     
+      <Tours tours ={tours} removeTour={removeTour}></Tours>
+    </div>
+  )
+};
+
+export default App;
